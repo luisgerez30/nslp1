043aeb2a335181804dee9ed179df29efb5271390
Rojas Roy <rojasroy89@gmail.com>
2021-06-02 20:10:04 -0300

    Revert "a"
    This reverts commit 2e3dae68cacd127075008159280d32d2326ea0d1.

--- nslp1/App_Code/Model.Designer.cs
+++ nslp1/App_Code/Model.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\Roy.DESKTOP-HC3RFIP\source\repos\nslp1\nslp1\App_Code\Model.edmx'. 
+﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\Lucho\source\repos\nslp1\nslp1\App_Code\Model.edmx'. 
 // Para habilitar la generación de código heredada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
 // por 'ObjectContext heredado'. Esta propiedad está disponible en la ventana Propiedades cuando se abre
 // el modelo en el diseñador.
